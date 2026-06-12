f0a5e8594f4341db5ed20154419834a5f91de4a0
Michael Jones <michael.jones90@me.com>
2026-06-12 17:53:20 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-Edutainment/HackingWithSwiftUI_EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_EdutainmentApp.swift
+//  HackingWithSwiftUI-Edutainment
+//
+//  Created by Michael Jones on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
